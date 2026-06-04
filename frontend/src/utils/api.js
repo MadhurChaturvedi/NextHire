8eaba76f46ef61d28e6f912f1df18679e0a6b5cf
madhur <madhurchaturvedi2000@gmail.com>
2026-06-04 07:43:14 +0530

    bug fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portfolio-fyv1.onrender.com" || "http://localhost:5000/api",
+  baseURL: "https://next-gger.onrender.com" || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
