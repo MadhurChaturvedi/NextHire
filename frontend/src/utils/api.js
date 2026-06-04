5f78d6b340b3203204951f2dea2d1bfd3b83b7e5
madhur <madhurchaturvedi2000@gmail.com>
2026-06-04 07:35:50 +0530

    bug fixd

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: "https://portfolio-fyv1.onrender.com" || "http://localhost:5000/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Automatically inject JWT token into request headers
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -18,7 +18,7 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
